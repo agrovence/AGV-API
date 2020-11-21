ecf60985e7a69411727268fa258c2cf4d303a0c7
João Lenon <lenonsec7@gmail.com>
2020-11-21 13:14:34 -0300

    feat: Finalized API  🎉🎉

---
+++ start/routes/client.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+const Config = use('Config')
+
+Route.group(() => {
+  /**
+   * Product Resource Routes
+   */
+  Route.get('products', 'ProductController.index')
+  Route.get('products/:id', 'ProductController.show')
+})
+  .prefix(`${Config.get('app.prefix')}`)
+  .namespace('Client')
